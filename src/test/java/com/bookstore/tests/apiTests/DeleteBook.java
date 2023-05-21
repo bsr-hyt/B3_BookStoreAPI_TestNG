10a6cf34cb94a962f33a811f2a08df09e27609fc
busra <busrahyt42@gmail.com>
2023-05-21 23:12:54 +0300

    DeleteBook class has been added.

---
+++ src/test/java/com/bookstore/tests/apiTests/DeleteBook.java
@@ -0,0 +1,40 @@
+package com.bookstore.tests.apiTests;
+
+import com.bookstore.tests.TestBase;
+import com.bookstore.utilities.ConfigurationReader;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DeleteBook extends TestBase {
+
+    @Test
+    public void deleteBook(){
+
+        /**{
+         "isbn": "string",
+         "userId": "string"
+         }*/
+
+        String body = "{\n" +
+                "  \"isbn\": \""+ ConfigurationReader.get("isbn8") +"\",\n" +
+                "  \"userId\": \""+ConfigurationReader.get("userID")+"\"\n" +
+                "}";
+
+        response = RestAssured.given()
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .body(body)
+                .header("Authorization","Bearer "+ConfigurationReader.get("token"))
+                .when().log().all()
+                .delete("/BookStore/v1/Book")
+                .prettyPeek();
+    }
+
+    @Test
+    public void verifyDeletingBook(){
+        Assert.assertEquals(response.statusCode(),204);
+    }
+
+}
